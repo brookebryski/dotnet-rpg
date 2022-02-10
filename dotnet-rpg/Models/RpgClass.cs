ec215cbd0dba8c39319f77043c46b0d9debcfd54
github username <github email address>
2022-02-10 15:06:01 -0600

    initial commit. First route, get, with mock knight character

---
+++ dotnet-rpg/Models/RpgClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight,
+
+        Mage,
+
+        Cleric
+    }
+}
