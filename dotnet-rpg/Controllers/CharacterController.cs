4b1bbbd40ad6bb9ebc9b0d04ec0ee365a5b836a1
github username <github email address>
2022-02-11 15:43:52 -0600

    async

--- dotnet-rpg/Controllers/CharacterController.cs
+++ dotnet-rpg/Controllers/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
@@ -21,7 +22,7 @@ namespace dotnet_rpg.Controllers
         // https://localhost:5001/Character/GetAll
 
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get()
+        public async Task<ActionResult<List<Character>>> Get()
         {
             return Ok(_characterService.GetAllCharacters());
         }
@@ -29,13 +30,13 @@ namespace dotnet_rpg.Controllers
        // https://localhost:5001/Character/{id}
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id)
+        public async Task<ActionResult<Character>> GetSingle(int id)
         {
             return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
         {
            
             return Ok(_characterService.AddCharacter(newCharacter));
