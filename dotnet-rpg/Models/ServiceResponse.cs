509e874013041983848227d3364c7fdb6a62a2fb
github username <github email address>
2022-02-14 10:45:06 -0600

    built add character and get character dtos

--- dotnet-rpg/Models/ServiceResponse.cs
+++ dotnet-rpg/Models/ServiceResponse.cs
@@ -1,5 +1,9 @@
-﻿using System;
-namespace dotnet_rpg.Models
+﻿namespace dotnet_rpg.Models
+
+// Questions for Lord Charles:
+// I still do not really get the point
+// Why are we setting the message to null? How do we get a message then?
+
 {
     public class ServiceResponse<T>
     {
